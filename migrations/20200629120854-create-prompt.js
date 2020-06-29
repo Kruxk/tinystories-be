a55c8bfb6979df988ee958a10ad23f8c40915fef
Kruxk <reinko.brink@gmail.com>
2020-06-29 14:15:37 +0200

    generated models

---
+++ migrations/20200629120854-create-prompt.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('prompts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.NUMBER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('prompts');
+  }
+};
---
+++ models/comment.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  comment.init({
+    userId: DataTypes.NUMBER,
+    bookId: DataTypes.NUMBER,
+    storyId: DataTypes.NUMBER,
+    isBook: DataTypes.BOOLEAN,
+    content: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'comment',
+  });
+  return comment;
+};
