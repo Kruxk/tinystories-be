22e3468e6e7e6e2fb5898ef4e999c8ef4a5ed40c
Kruxk <reinko.brink@gmail.com>
2020-06-29 15:56:06 +0200

    added dummy data

---
+++ seeders/20200629133024-some-users.js
@@ -0,0 +1,45 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "Reinko",
+          email: "reinko@tinystories.com",
+          token: "reinko",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Til",
+          email: "til@tinystories.com",
+          token: "til",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete("users", null, {});
+  },
+};
---
+++ seeders/20200629134452-a-story.js
@@ -0,0 +1,43 @@
+"use strict";
+const User = require("../models").user;
+const Prompt = require("../models").prompt;
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const reinko = await User.findOne({
+      where: { name: "Reinko" },
+    });
+    const prompt = await Prompt.findOne({
+      where: { userId: reinko.id },
+    });
+
+    return queryInterface.bulkInsert("stories", [
+      {
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam est inventore facilis neque cum cupiditate.",
+        userId: reinko.id,
+        promptId: prompt.id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
