3eee0020c04392606c75ee6d137acbe632ced564
Kruxk <reinko.brink@gmail.com>
2020-06-30 15:40:55 +0200

    added login route

--- routers/users.js
+++ routers/users.js
@@ -12,4 +12,28 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Please provide both email and password" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || password !== user.token) {
+      return res.status(400).send({
+        message: "User with that email not found or password incorrect",
+      });
+    }
+    return res.status(200).send({ ...user });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
 module.exports = router;
