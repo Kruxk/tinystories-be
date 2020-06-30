5f52abb523299c35b49118085f7f0466b9f59803
Kruxk <reinko.brink@gmail.com>
2020-06-30 16:11:09 +0200

    added routes for login and signup no auth yet

--- routers/users.js
+++ routers/users.js
@@ -36,4 +36,29 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
+router.post("/signup", async (req, res) => {
+  const { email, password, name, picture } = req.body;
+  if (!email || !password || !name) {
+    return res.status(400).send("Please provide an email, password and a name");
+  }
+  try {
+    const newUser = await User.create({
+      email,
+      token: password,
+      name,
+      picture,
+    });
+
+    res.status(201).json({ ...newUser });
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      return res
+        .status(400)
+        .send({ message: "There is an existing account with this email" });
+    }
+
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
 module.exports = router;
